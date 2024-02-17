21dcc3a18bb23c6d45b788228e299841430c7248
Tammyranae Galzote <tammyranae@Tammyranaes-MBP.lan>
2024-02-17 11:33:04 -1000

    Initial commit

---
+++ public class Main {.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Keep Calm");
+        System.out.print("and");
+        System.out.print("Carry on");
+    }
+}
